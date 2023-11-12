f554968feb0cf2248fa623abc12f924f6e96eeee
Enrique <115951243+PEVF@users.noreply.github.com>
2023-11-11 22:27:49 -0600

    Update AppFooter.vue

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -13,7 +13,7 @@ const logoUrl = computed(() => {
     <div class="layout-footer">
         <img :src="logoUrl" alt="Logo" height="20" class="mr-2" />
         by
-        <span class="font-medium ml-2">Paulino Enrique N. Velazquez</span>
+        <span class="font-medium ml-2">Paulino Enrique & Keyla Yoselin</span>
     </div>
 </template>
 <style lang="scss" scoped></style>
