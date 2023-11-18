521d0de0f77147021dd37ea244940b12c086dfc5
Enrique <115951243+PEVF@users.noreply.github.com>
2023-11-18 01:19:30 -0600

    Update FloatLabel.vue

--- src/views/uikit/FloatLabel.vue
+++ src/views/uikit/FloatLabel.vue
@@ -1,42 +1,116 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onBeforeMount, onMounted, computed } from 'vue';
+import CountryService from '@/service/CountryService';
 
-const calendarValue = ref(null);
+const countryService = new CountryService();
+const loading = ref([false, false, false]);
+onMounted(() => {
+    countryService.getCountries().then((data) => (countries.value = data));
+});
+
+const load = (index) => {
+    loading.value[index] = true;
+    setTimeout(() => (loading.value[index] = false), 1000);
+};
+</script>
+
+<script>
+import axios from "axios";
+import { defineComponent } from 'vue';
+export default defineComponent({
+    data() {
+        return {
+            empleados: [],
+            id_emp: null,
+            message: "No has ingresado su ID del empleado",
+            empleado: {}
+        };
+
+    },
+    mounted() {
+        axios.get("https://dummy.restapiexample.com/api/v1/employees", {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            }
+        }).then((response) => {
+            this.empleados = response.data.data;
+        })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+
+    methods: {
+        Cons() {
+
+            if (this.id_emp === null) {
+                this.$toast.add({ severity: 'warn', summary: 'Favor de ingresar el ID del empleado', detail: 'No has Ingresado su ID del empleado', life: 5000 });
+
+            } else {
+                axios.get(`https://dummy.restapiexample.com/api/v1/employee/${this.id_emp}`, {
+
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                    }
+                }).
+                    then((response) => {
+                        this.empleado = response.data.data;
+                    }).catch((error) => {
+
+                        this.$toast.add({ severity: 'warn', summary: 'Error interno de la API', detail: 'LA API GENERA UN "TIPO ERROR" AL BUSCAR LOS DATOS DE UN EMPLEADO EN ESPECIFICO!', life: 3000 });
+                        console.log(error);
+                    });
+            }
 
 
-const dropdownItems = ref([
-    { name: 'Option 1', code: 'Option 1' },
-    { name: 'Option 2', code: 'Option 2' },
-    { name: 'Option 3', code: 'Option 3' }
-]);
-
-const dropdownItem = ref(null);
+        }
+    }
+});
 </script>
 
 <template>
-    <div class="grid">
-        <div class="col-12 md:col-12">
-            <div class="card p-fluid">
-                <h5>Consumir API REST Empleado</h5>
-                <div class="field">
-                    <label for="nom">Nombre(S):</label>
-                    <InputText id="nom" type="text" />
-                </div>
-                <div class="field">
-                    <label for="salario">Salario:</label>
-                    <InputText id="salario" type="text" />
-                </div>
-                <div class="field">
-                    <label for="edad">Edad:</label>
-                    <InputText id="edad" type="text" />
-                </div>
-                <div class="field">
-                    <label for="idem">Id Emp:</label>
-                    <InputText id="idem" type="text" />
-                </div>
-                <div class="col-12 md:col-6">
-                    <Button label="Consultar datos" class="p-button-success mr-2 mb-2" icon="pi pi-search"/>
-                </div>
+    <div class="p-grid">
+        <Toast />
+        <div class="p-col-12">
+            <div class="card">
+                <Panel header="Consumiendo una API" style="height: 100%">
+                    <Toolbar class="p-mb-4">
+                        <template v-slot:start && #body="slotProps">
+                            <InputNumber id="number-input" v-model="id_emp" size="40" placeholder="ID del empleado"
+                                style="margin-right: 10px;"></InputNumber>
+                            <Button type="button" class="p-float-label" label="  Buscar" icon="pi pi-search" iconPos="right"
+                                :loading="loading[1]" @click="load(1) & Cons()" style="margin-right: 50px;" />
+                            <InputText id="nombre" type="text" :value="empleado.employee_name" size="40"
+                                placeholder="Nombre" style="margin-right: 10px;">
+                            </InputText>
+                            <InputText id="salario" type="text" :value="empleado.employee_salary" size="40"
+                                placeholder="Edad" style="margin-right: 10px;">
+                            </InputText>
+                            <InputText id="edad" type="text" :value="empleado.employee_age" size="40" placeholder="Sueldo"
+                                style="margin-right: 10px;">
+                            </InputText>
+                        </template>
+                        <br>
+                    </Toolbar>
+                    <hr>
+                    <div class="grid">
+                        <div class="col-12">
+                            <div class="card">
+                                <div>
+                                    <h3>empleados</h3>
+                                    <DataTable v-bind="value" :value="empleados" showGridlines paginator :rows="5"
+                                        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                                        :rowsPerPageOptions="[5, 10, 15, 20, 25]" tableStyle="min-width: 50rem"
+                                        currentPageReportTemplate="Visualizando {last} de {totalRecords} empleados!">
+                                        <Column field="employee_name" :sortable="true" header="Nombre"></Column>
+                                        <Column field="employee_salary" header="Salario"></Column>
+                                        <Column field="employee_age" header="Edad"></Column>
+                                    </DataTable>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Panel>
             </div>
         </div>
     </div>
