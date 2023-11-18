2ec369e1ae089e4de74d33ec7436666e370293a7
Enrique <115951243+PEVF@users.noreply.github.com>
2023-11-18 01:23:24 -0600

    Update AppMenu.vue

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -14,7 +14,7 @@ const model = ref([
             { label: 'Demo Clientes', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
             { label: 'Punto de Venta', icon: 'pi pi-fw pi-check-square', to: '/uikit/input' },
             { label: 'Consumir API', icon: 'pi pi-fw pi-bookmark', to: '/uikit/floatlabel' },
-            { label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', to: '/uikit/invalidstate' },
+            { label: 'Grafica API', icon: 'pi pi-fw pi-exclamation-circle', to: '/uikit/invalidstate' },
             { label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
             { label: 'Table', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
             { label: 'List', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
